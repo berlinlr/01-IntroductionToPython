b2824447384652096ce2c6086a7ef837a8a00010
berlinlr <berlinlr@rose-hulman.edu>
2019-03-06 11:05:25 -0500

    day 1

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -4,13 +4,14 @@ Your chance to explore Loops and Turtles!
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Aaron Wilkin, their colleagues, and Landen Berlin.
 """
+
 ########################################################################
 # DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,6 +29,7 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
 window = rg.TurtleWindow()
 red_boy = rg.SimpleTurtle('square')
 red_boy.pen = rg.Pen('red',3)
@@ -35,3 +37,23 @@ red_boy.speed = 15
 size = 100
 for k in range (4):
     red_boy.draw_square(size)
+    red_boy.pen_up()
+    red_boy.right(45)
+    red_boy.forward(10)
+    red_boy.left(45)
+    red_boy.pen_down()
+    size = size - 12
+
+blue_boy = rg.TurtleWindow()
+blue_boy = rg.SimpleTurtle('circle')
+blue_boy.pen = rg.Pen('blue',3)
+blue_boy.speed = 5
+size = 50
+for k in range (10):
+    blue_boy.draw_circle(size)
+    blue_boy.pen_up()
+    blue_boy.left(30)
+    blue_boy.forward(20)
+    blue_boy.right(30)
+    blue_boy.pen_down()
+    size = size - 8
